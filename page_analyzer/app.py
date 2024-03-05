5d16c8139256484b1eeccd2dbd14da4ff15e19c7
Евгений <prokofev_619@mail.ru>
2024-03-05 21:32:08 +0300

    create form

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import (
+    Flask, render_template, request, flash,
+)
+
 from dotenv import load_dotenv
 import os
+from .url import validate
 
 
 load_dotenv()
@@ -10,7 +14,16 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.get('/')
 def get_index():
-    return render_template('index.html')
+    return render_template('index.html'), 200
+
+
+@app.route('/urls', methods=["POST"])
+def add_url():
+    url = request.form.get('url')
+    error = validate(url)
+    if error:
+        flash(error, 'alert-danger')
+        return render_template('index.html'), 422
 
 
 if __name__ == '__main__':
---
+++ page_analyzer/url.py
@@ -0,0 +1,21 @@
+from urllib.parse import urlparse
+import validators
+
+
+MAX_URL_LENGHT = 255
+
+
+def validate(url):
+    error = None
+    if not url:
+        error = 'Отсутствует URL'
+    elif len(url) > MAX_URL_LENGHT:
+        error = f'URL превышает {MAX_URL_LENGHT} символов'
+    elif not validators.url(url):
+        error = 'Некорректный URL'
+    return error
+
+
+def normalize(url):
+    url_read = urlparse(url)
+    return url_read.sheme + '://' + url_read.netloc
