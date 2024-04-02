8abd2e0eeaf81cbbbb4ad77259697a92ff30ad0a
Евгений <prokofev_619@mail.ru>
2024-04-02 23:24:35 +0300

    change type cursor

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,5 +1,5 @@
 import psycopg2
-from psycopg2.extras import DictCursor
+from psycopg2.extras import DictCursor, NamedTupleCursor
 import os
 from dotenv import load_dotenv
 from datetime import datetime
@@ -10,13 +10,13 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-def get_connection(database_url):
+def get_conn(database_url):
     connection = psycopg2.connect(database_url)
     return connection
 
 
 def add_url_into_db(url):
-    with get_connection(DATABASE_URL).cursor(cursor_factory=DictCursor) as curs:
+    with get_conn(DATABASE_URL).cursor(cursor_factory=NamedTupleCursor) as curs:
         query = (
             'INSERT INTO urls '
             '(name, created_at) '
@@ -29,21 +29,21 @@ def add_url_into_db(url):
 
 
 def get_url_by_name(url):
-    with get_connection(DATABASE_URL).cursor(cursor_factory=DictCursor) as curs:
+    with get_conn(DATABASE_URL).cursor(cursor_factory=DictCursor) as curs:
         query = 'SELECT * FROM urls WHERE name = (%s)'
         curs.execute(query, (url,))
         return curs.fetchone()
 
 
 def get_url_by_id(id):
-    with get_connection(DATABASE_URL).cursor(cursor_factory=DictCursor) as curs:
+    with get_conn(DATABASE_URL).cursor(cursor_factory=DictCursor) as curs:
         query = 'SELECT * FROM urls WHERE id = (%s)'
         curs.execute(query, (id,))
         return curs.fetchone()
 
 
 def get_urls_list():
-    with get_connection(DATABASE_URL).cursor(cursor_factory=DictCursor) as curs:
+    with get_conn(DATABASE_URL).cursor(cursor_factory=DictCursor) as curs:
         query = 'SELECT * FROM urls'
         curs.execute(query, (id, ))
         data = curs.fetchall()
